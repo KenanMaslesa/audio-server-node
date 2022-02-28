3dcafc1b583961b225020c98fc3ae239210fba3d
Kenan Maslesa <kenoo.maslesa@gmail.com>
2022-02-28 12:47:59 +0100

    .

--- server.js
+++ server.js
@@ -8,7 +8,5 @@ app.use((req, res, next) => {
 
 app.use('', express.static(path.join(__dirname, 'public')));
 
-
 const PORT = process.env.PORT || 3000;
-//http://localhost:3000/audio/afve.mp3
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
